9ecb9ac82c57b859961d489c181878de7371218b
Am-all1 <amandine.al@live.fr>
2022-05-23 13:52:55 +0200

    style bouton

--- src/views/ActuView.vue
+++ src/views/ActuView.vue
@@ -4,15 +4,20 @@
     <nav></nav>
     <div id="logInAcc">
       <div>
-        <button @click="$router.push('/Login')">Connexion</button>
+        <button @click="$router.push('/Login')" class="buttonStyle" id="btn1">
+          Connexion
+        </button>
+        <br />
+        <br />
+      </div>
+
+      <div>
+        <button class="buttonStyle" id="btn2">Mon profil</button>
       </div>
       <br />
       <br />
       <div>
-        <button>Mon profil</button>
-      </div>
-      <div>
-        <button>Déconnexion</button>
+        <button class="buttonStyle" id="btn3">Déconnexion</button>
       </div>
     </div>
     <Recuperer :emitprop="this.test"></Recuperer>
@@ -82,5 +87,27 @@ export default {
 #btn {
   font-family: initial;
   letter-spacing: 1.8px;
+}
+.buttonStyle {
+  background-color: rgba(25, 25, 112, 0.684);
+  color: white;
+  padding: 5px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  -webkit-transition-duration: 0.4s; /* Safari */
+  transition-duration: 0.4s;
+}
+#btn1:hover {
+  box-shadow: 0 5px 5px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.19);
+}
+#btn2:hover {
+  box-shadow: 0 5px 5px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.19);
+}
+#btn3:hover {
+  box-shadow: 0 5px 5px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.19);
 }
 </style>
